5b9844a5d356efbb90a60d0c5cc163319d5ac03a
Trent Roesbery <xtrent777x@gmail.com>
2021-09-09 17:08:32 -0600

    npm packages

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const mysql = require('mysql2');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'test'
+});
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
