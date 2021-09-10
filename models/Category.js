8878dd8ae05cdb95a485fc243c3c359df5db59fd
Trent Roesbery <xtrent777x@gmail.com>
2021-09-09 20:30:57 -0600

    category complete

--- models/Category.js
+++ models/Category.js
@@ -7,8 +7,17 @@ class Category extends Model {}
 Category.init(
   {
     // define columns (id - integer - doesnt allow null- set primary - uses auto)
-
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     // (category_name string - doesnt allow null)
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
